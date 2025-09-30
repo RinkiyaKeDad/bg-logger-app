15136feeb9e3c0294a05e88bc9357e8ed4331be7
Arsh Sharma <arshsharma461@gmail.com>
2025-09-30 12:34:43 +0530

    Initial Commit

---
+++ BgLogger/BgLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BgLoggerApp.swift
+//  BgLogger
+//
+//  Created by Arsh Sharma on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BgLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
